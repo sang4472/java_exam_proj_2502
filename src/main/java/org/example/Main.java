3ab5469f49fc2a80489ee339da0f8e114348fd27
sang4472 <shk4472@gmail.com>
2025-02-21 22:19:01 +0900

    게시물 저장 힌트

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,31 @@
 package org.example;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-
 public class Main {
   public static void main(String[] args) {
-    System.out.println("==프로그램 시작==");
-    Scanner sc = new Scanner(System.in);//스캐너 객체 만드는법,
-
-    System.out.print("명령어) ");
-    String cmd = sc.nextLine();//입력하는것,띄어쓰기 까지 받는게 가능
-    System.out.printf("입력된 명령어 : %s\n", cmd);
-
-
-    sc.close();//스캐너 닫기(안하면오류)
-
-
-
-    System.out.println("==프로그램 끝==");
+    Scanner sc = new Scanner(System.in);
+    List<Integer> nums = new ArrayList<>();
+    while (true) {
+      System.out.printf("명령어) ");
+      String cmd = sc.nextLine();//입력하는것,띄어쓰기 까지 받는게 가능
+      cmd = cmd.trim();
+      if ( cmd.length() == 0 ) {//cmd의 길이가 0이여도 통과, 넘어가겠다
+        continue;
+      }
+      if ( cmd.equals("exit") ) {//입력란에 exit 입력시 컴파일 종료
+        break;
+      }
+      if ( cmd.equals("num") ) {//equals:객체 간의 내용(value)이 같은지 비교하는 메서드, num입력시 숫자로만 입력가능
+        System.out.printf("숫자 : ");
+        int num = sc.nextInt();
+        sc.nextLine();
+        nums.add(num);
+      }
+      else if ( cmd.equals("nums") ) {//
+        System.out.printf("숫자들 : %s\n", nums.toString());
+      }
+    }
+    sc.close();
   }
 }
