a7b5fd90f33e16fc2a9f7d6fdac5028c8858d679
sang4472 <shk4472@gmail.com>
2025-02-21 22:39:17 +0900

    게시물 저장하기 전 단계, 구조만

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,42 @@
 package org.example;
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    List<Integer> nums = new ArrayList<>();
-    while (true) {
-      System.out.printf("명령어) ");
+    System.out.println("== 프로그램 시작 ==");
+    Scanner sc = new Scanner(System.in);//스캐너 객체 만드는법
+    int lastArticleId = 0;
+
+    while ( true ) {
+      System.out.print("명령어) ");
       String cmd = sc.nextLine();//입력하는것,띄어쓰기 까지 받는게 가능
-      cmd = cmd.trim();
-      if ( cmd.length() == 0 ) {//cmd의 길이가 0이여도 통과, 넘어가겠다
+      cmd = cmd.trim();//양옆공백제거
+
+      if ( cmd.length() == 0 ) {//길이가 0이어도 통과, 넘어가라
         continue;
       }
-      if ( cmd.equals("exit") ) {//입력란에 exit 입력시 컴파일 종료
+
+      if ( cmd.equals("exit") ) {//exit 입력시 컴파일 종료
         break;
       }
-      if ( cmd.equals("num") ) {//equals:객체 간의 내용(value)이 같은지 비교하는 메서드, num입력시 숫자로만 입력가능
-        System.out.printf("숫자 : ");
-        int num = sc.nextInt();
-        sc.nextLine();
-        nums.add(num);
+      else if ( cmd.equals("article list") ) {//입력란에 article list 입력시 다음 출력
+        System.out.println("게시물이 없습니다.");
       }
-      else if ( cmd.equals("nums") ) {//
-        System.out.printf("숫자들 : %s\n", nums.toString());
+      else if ( cmd.equals("article write") ) {//입력란에 article write 입력시 제목과 내용이 출력하면서 입력가능
+        int id = lastArticleId + 1;//lastArticleId를 0으로 초기값을 설정을 해두어서 +1
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+
+        lastArticleId = id;//1저장,2저장,3저장
+
+        System.out.printf("%d번 글이 작성되었습니다.\n", id);
       }
     }
-    sc.close();
+
+    sc.close();//스캐너 닫기
+    System.out.println("== 프로그램 끝 ==");
   }
 }
